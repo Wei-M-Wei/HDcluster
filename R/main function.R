ad6c730314d0c692933169cddb2b92dcfc09ade4
Wei-M-Wei <wei.miao@kuleuven.be>
2025-03-23 19:56:02 +0100

    initial commit
    initial files

---
+++ R/main function.R
@@ -0,0 +1,205 @@
+# Estimator Function with Optional Cross-Fitting
+estimator_dc <- function(formula, data, index, CF = FALSE, init = 30) {
+  if (!CF) {
+    est_without_CF(formula, data, index, init)
+  } else {
+    est_with_CF(formula, data,index, init)
+  }
+}
+
+# General Clustering Function
+cluster_general <- function(Y, X_list, N, T, init, type = "long", groups = NULL) {
+  if (type == "long") {
+    Y_mean <- rowMeans(Y)
+    X_means <- lapply(X_list, rowMeans)
+    data <- cbind(Y_mean, do.call(cbind, X_means))
+    variance <- sum(sapply(1:T, function(t) {
+      norm(cbind(Y[, t], sapply(X_list, function(X) X[, t])) - data, type = "F")^2
+    })) / (N * T^2)
+    dim_size <- N
+  } else if (type == "tall") {
+    Y_mean <- colMeans(Y)
+    X_means <- lapply(X_list, colMeans)
+    data <- cbind(Y_mean, do.call(cbind, X_means))
+    variance <- sum(sapply(1:N, function(i) {
+      norm(cbind(Y[i, ], sapply(X_list, function(X) X[i, ])) - data, type = "F")^2
+    })) / (N^2 * T)
+    dim_size <- T
+  } else {
+    stop("Invalid type. Use 'long' for rows or 'tall' for columns.")
+  }
+
+  # Clustering logic
+  if (!is.null(groups)) {
+    clusters <- groups
+    k_result <- kmeans(data, centers = clusters, algorithm = "Hartigan-Wong", nstart = init)
+  } else {
+    clusters <- 1
+    repeat {
+      k_result <- kmeans(data, centers = clusters, algorithm = "Hartigan-Wong", nstart = init)
+      if (k_result$tot.withinss / dim_size <= variance) break
+      clusters <- clusters + 1
+    }
+  }
+  list(res = k_result, clusters = clusters)
+}
+
+# Helper Function to Reshape Data into a Matrix
+reshape_to_matrix <- function(data, id_var, time_var, value_var) {
+  reshaped <- reshape(data[, c(id_var, time_var, value_var)],
+                      idvar = id_var,
+                      timevar = time_var,
+                      direction = "wide")
+  as.matrix(reshaped[, -1])  # Drop the 'id' column and return the matrix
+}
+
+
+# Function to recode indices
+recode_indices <- function(data, index) {
+
+  unique_id <- sort(unique(data[,c(index[1])]))
+  index_to_id <- setNames(1:length(unique_id), unique_id)
+  data$id <- index_to_id[as.character(data[,c(index[1])])]
+
+  unique_time <- sort(unique(data[,c(index[2])]))
+  index_to_time <- setNames(1:length(unique_time), unique_time)
+  data$time <- index_to_time[as.character(data[,c(index[2])])]
+
+  data
+}
+
+# Function to Split Data into Dynamic Folds
+split_indices <- function(dim, folds) {
+  indices <- seq_len(dim)
+  split(seq_len(dim), cut(indices, folds, labels = FALSE))
+}
+
+# Estimator Without Cross-Fitting
+est_without_CF <- function(formula, data, index, init) {
+  formula_vars <- all.vars(formula)
+  dependent_var <- formula_vars[1]
+  independent_vars <- formula_vars[-1]
+
+  data <- recode_indices(data,index)
+
+  Y <- reshape_to_matrix(data, "id", "time", dependent_var)
+  X_list <- lapply(independent_vars, function(var) reshape_to_matrix(data, "id", "time", var))
+  X_combined <- do.call(cbind, X_list)
+
+  N <- nrow(Y)
+  T <- ncol(Y)
+
+  clusteri <- cluster_general(Y, X_list, N, T, init, type = "long")
+  G <- clusteri$clusters
+  klong <- clusteri$res
+
+  clustert <- cluster_general(Y, X_list, N, T, init, type = "tall")
+  C <- clustert$clusters
+  ktall <- clustert$res
+
+  Du <- matrix(0, N, G)
+  Dv <- matrix(0, T, C)
+
+  for (j in seq_len(G)) {
+    Du[, j] <- as.numeric(klong$cluster == j)
+  }
+  for (j in seq_len(C)) {
+    Dv[, j] <- as.numeric(ktall$cluster == j)
+  }
+
+  Mu <- diag(N) - Du %*% solve(t(Du) %*% Du) %*% t(Du)
+  Mv <- diag(T) - Dv %*% solve(t(Dv) %*% Dv) %*% t(Dv)
+
+  tY <- c(Mu %*% Y %*% Mv)
+  tX_combined <- do.call(cbind, lapply(X_list, function(X) c(Mu %*% X %*% Mv)))
+  new_data <- data.frame(tY, tX_combined, data[, "id"], data[, "time"])
+  colnames(new_data) <- c(formula_vars, "id", "time")
+
+  list(res = plm(formula, data = new_data, index = c("id", "time"), model = "pooling"), G = G, C = C)
+}
+
+# Estimator with Cross-Fitting
+est_with_CF <- function(formula, data, index, init, folds = 2) {
+  formula_vars <- all.vars(formula)
+  dependent_var <- formula_vars[1]
+  independent_vars <- formula_vars[-1]
+
+  data <- recode_indices(data,index)
+
+  Y <- reshape_to_matrix(data, "id", "time", dependent_var)
+  X_list <- lapply(independent_vars, function(var) reshape_to_matrix(data, "id", "time", var))
+
+  N <- nrow(Y)
+  T <- ncol(Y)
+  N_folds <- split_indices(N, folds)
+  T_folds <- split_indices(T, folds)
+
+  fold_combinations <- expand.grid(N_fold = seq_along(N_folds), T_fold = seq_along(T_folds))
+
+  Gd <- numeric(nrow(fold_combinations))
+  Cd <- numeric(nrow(fold_combinations))
+  projection_matrices <- list()
+
+  for (i in seq_len(nrow(fold_combinations))) {
+    N_idx <- N_folds[[fold_combinations$N_fold[i]]]
+    T_idx <- T_folds[[fold_combinations$T_fold[i]]]
+
+    dim_N <- length(N_idx)
+    dim_T <- length(T_idx)
+
+    Y_comp_long <- Y[N_idx, -T_idx]
+    X_comp_long_list <- lapply(X_list, function(X) X[N_idx, -T_idx])
+
+    Y_comp_tall <- Y[-N_idx, T_idx]
+    X_comp_tall_list <- lapply(X_list, function(X) X[-N_idx, T_idx])
+
+    clusteri <- cluster_general(Y_comp_long, X_comp_long_list, nrow(Y_comp_long), ncol(Y_comp_long), init, type = "long")
+    clustert <- cluster_general(Y_comp_tall, X_comp_tall_list, nrow(Y_comp_tall), ncol(Y_comp_tall), init, type = "tall")
+
+    G <- clusteri$clusters
+    C <- clustert$clusters
+
+    Du <- matrix(0, dim_N, G)
+    Dv <- matrix(0, dim_T, C)
+
+    for (j in seq_len(G)) {
+      Du[, j] <- as.numeric(clusteri$res$cluster == j)
+    }
+    for (j in seq_len(C)) {
+      Dv[, j] <- as.numeric(clustert$res$cluster == j)
+    }
+
+    Mu <- diag(1, dim_N) - Du %*% solve(t(Du) %*% Du) %*% t(Du)
+    Mv <- diag(1, dim_T) - Dv %*% solve(t(Dv) %*% Dv) %*% t(Dv)
+
+    projection_matrices[[i]] <- list(Mu = Mu, Mv = Mv, N_slice = N_idx, T_slice = T_idx)
+    Gd[i] <- G
+    Cd[i] <- C
+  }
+
+  df <- 0
+
+  projection <- function(X) {
+    for (i in seq_along(projection_matrices)) {
+      proj <- projection_matrices[[i]]
+      X[proj$N_slice, proj$T_slice] <- proj$Mu %*% X[proj$N_slice, proj$T_slice] %*% proj$Mv
+    }
+    X
+  }
+
+  Y <- projection(Y)
+  X_list <- lapply(X_list, projection)
+
+  for (i in seq_along(projection_matrices)) {
+    proj <- projection_matrices[[i]]
+    df <- df + (length(proj$N_slice) - Gd[i]) * (length(proj$T_slice) - Cd[i])
+  }
+
+  tY <- c(Y)
+  tX_combined <- do.call(cbind, lapply(X_list, function(X) c(X)))
+  new_data <- data.frame(tY, tX_combined, data[, "id"], data[, "time"])
+  colnames(new_data) <- c(formula_vars, "id", "time")
+
+  list(res = plm(formula, data = new_data, index = c("id", "time"), model = "pooling"), df = df)
+}
+
